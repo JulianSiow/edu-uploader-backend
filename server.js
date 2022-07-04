dd129a507d72f02055c036af4d2697278a5e2f5a
Julian Siow <julian.k.siow@gmail.com>
2022-07-04 21:49:55 +0200

    serves proper files on index route

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = __dirname + '/app/views/';
-const files = require('./app/routes/routes');
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = __dirname + "/app/dist/";
+const files = require("./app/routes/routes");
 
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:8081'
+  origin: "http://localhost:8081",
 };
 
-app.use(fileUpload({
-    createParentPath: true
-}));
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 
-app.use(express.static(path))
+app.use(express.static(path));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', files)
+app.use(express.static(path));
+app.use("/", files);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
